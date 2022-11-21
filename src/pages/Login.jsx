03214248b215f0c27768f0cb9b32c2784c0b2902
yerinjo <dpfls5645@khu.ac.kr>
2022-11-22 03:01:11 +0900

    feat: modify login/signup page 1

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import './login.css';
+
+const Login = () => (
+  <div className="form signup">
+    <div className="form-header">
+      <div className="show-signup">Sign Up</div>
+      <div className="show-signin">Sign In</div>
+      <div className="show-reset">Reset</div>
+    </div>
+  </div>
+);
+
+export default Login;
