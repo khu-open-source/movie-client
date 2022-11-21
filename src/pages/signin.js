34ab677d77b742e6007c3dce850d595be37eb9f4
yerinjo <dpfls5645@khu.ac.kr>
2022-11-22 03:20:17 +0900

    feat: add signin/up js #1

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -9,3 +9,17 @@ document.getElementsByClassName('show-signup')[0].addEventListener('click', () =
   form.classList.add('signup');
   document.getElementById('submit-btn').innerText = 'Sign Up';
 });
+document.getElementsByClassName('show-signin')[0].addEventListener('click', () => {
+  const form = document.getElementsByClassName('form')[0];
+  resetClass(form, 'signup');
+  resetClass(form, 'reset');
+  form.classList.add('signin');
+  document.getElementById('submit-btn').innerText = 'Sign In';
+});
+document.getElementsByClassName('show-reset')[0].addEventListener('click', () => {
+  const form = document.getElementsByClassName('form')[0];
+  resetClass(form, 'signup');
+  resetClass(form, 'signin');
+  form.classList.add('reset');
+  document.getElementById('submit-btn').innerText = 'Reset password';
+});
