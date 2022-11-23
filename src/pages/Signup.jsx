7fe84afe2767eb4de8dea1352a6bcc6d3dbb002c
yerinjo <dpfls5645@khu.ac.kr>
2022-11-23 17:51:49 +0900

    fix: modify file name #1

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,31 @@
+import './login.css';
+
+const Login = () => (
+  <div className="form signup">
+    <div className="form-header">
+      <div className="show-signup">Sign Up</div>
+      <div className="show-signin">Sign In</div>
+      <div className="show-reset">Reset</div>
+    </div>
+    <div className="arrow" />
+    <div className="form-elements">
+      <div className="form-element">
+        <input type="email" placeholder="Email" />
+      </div>
+      <div className="form-element">
+        <input type="text" placeholder="User ID" />
+      </div>
+      <div className="form-element">
+        <input type="password" placeholder="Password" />
+      </div>
+      <div className="form-element">
+        <input type="password" placeholder="Confirm password" />
+      </div>
+      {/* <div className='form-element'>
+                <button id='submit-btn'>Sign Up</button>
+            </div> */}
+    </div>
+  </div>
+);
+
+export default Login;
