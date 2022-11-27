0d730905a6223c9fa4b184e9ff5331739b608747
yerinjo <dpfls5645@khu.ac.kr>
2022-11-27 14:25:27 +0900

    fix: div->nav,form tag change #1

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,29 +1,29 @@
 /* eslint-disable react/button-has-type */
-import './login.css';
+import './signup.css';
 
 const Signup = () => (
   <div className="form signup">
-    <div className="form-header">
-      <div className="show-signup">Sign Up</div>
-      <div className="show-signin">Sign In</div>
-    </div>
+    <nav className="form-header">
+      <li className="show-signup">Sign Up</li>
+      <li className="show-signin">Sign In</li>
+    </nav>
     <div className="arrow" />
     <div className="form-elements">
-      <div className="form-element">
+      <form className="form-element">
         <input type="email" placeholder="Email" />
-      </div>
-      <div className="form-element">
+      </form>
+      <form className="form-element">
         <input type="text" placeholder="User ID" />
-      </div>
-      <div className="form-element">
+      </form>
+      <form className="form-element">
         <input type="password" placeholder="Password" />
-      </div>
-      <div className="form-element">
+      </form>
+      <form className="form-element">
         <input type="password" placeholder="Confirm password" />
-      </div>
-      <div className="form-element">
-        <button id="submit-btn">Sign Up</button>
-      </div>
+      </form>
+      <form className="form-element">
+        <button className="submit-btn">Sign Up</button>
+      </form>
     </div>
   </div>
 );
