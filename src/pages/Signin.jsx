6d614e86e0ded1acee8b43ac746846be60d8a8ca
yerinjo <dpfls5645@khu.ac.kr>
2022-11-27 14:25:09 +0900

    fix: div->form tag change #1

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -3,24 +3,21 @@ import './login.css';
 
 const Signin = () => (
   <div className="form signup">
-    <div className="form-header">
-      <div className="show-signup">Sign Up</div>
-      <div className="show-signin">Sign In</div>
-    </div>
+    <nav className="form-header">
+      <li className="show-signup">Sign Up</li>
+      <li className="show-signin">Sign In</li>
+    </nav>
     <div className="arrow" />
     <div className="form-elements">
-      <div className="form-element">
+      <form className="form-element">
         <input type="text" placeholder="User ID" />
-      </div>
-      <div className="form-element">
+      </form>
+      <form className="form-element">
         <input type="password" placeholder="Password" />
-      </div>
-      <div className="form-element">
-        <input type="password" placeholder="Confirm password" />
-      </div>
-      <div className="form-element">
-        <button id="submit-btn">Sign In</button>
-      </div>
+      </form>
+      <form className="form-element">
+        <button id="submit-btn">Sign In</button>{' '}
+      </form>
     </div>
   </div>
 );
