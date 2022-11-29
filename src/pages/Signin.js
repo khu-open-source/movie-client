dedd22071dadcba382a27862ff943c63efd1a0de
yerinjo <dpfls5645@khu.ac.kr>
2022-11-29 11:09:08 +0900

    feat: link to signup/in #13

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,11 +1,17 @@
 /* eslint-disable react/button-has-type */
 import './login.css';
 
+import { Link } from 'react-router-dom';
+
 const Signin = () => (
   <div className="form signup">
     <nav className="form-header">
-      <li className="show-signup">Sign Up</li>
-      <li className="show-signin">Sign In</li>
+      <li className="show-sign">
+        <Link to="/SignIn">Sign In</Link>
+      </li>
+      <li className="show-sign">
+        <Link to="/SignUp">Sign Up</Link>
+      </li>
     </nav>
     <div className="arrow" />
     <div className="form-elements">
