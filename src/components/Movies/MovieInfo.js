fa1c971fed03cb49b2a5075cfe24ff869d420083
yerinjo <dpfls5645@khu.ac.kr>
2022-12-03 16:13:12 +0900

    feat: add className, poster block #movieinfo

--- src/components/Movies/MovieInfo.js
+++ src/components/Movies/MovieInfo.js
@@ -1,9 +1,14 @@
+import './movieinfo.css';
+
 const MovieInfo = (props) => (
-  <li>
-    <h2>{props.title}</h2>
-    <div>{props.genre}</div>
-    <div>{props.rank}</div>
-  </li>
+  <div className="movie-info">
+    <li className="movie-list">
+      <div className="movie-poster">{props.poster}</div>
+      <h2 className="movie-title">{props.title}</h2>
+      <div className="movie-genre">{props.genre}</div>
+      <div className="movie-rank">{props.rank}</div>
+    </li>
+  </div>
 );
 
 export default MovieInfo;
