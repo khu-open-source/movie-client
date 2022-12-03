4690d3de825b967f58505a5d568a69aeb91ccd23
yerinjo <dpfls5645@khu.ac.kr>
2022-12-03 16:14:11 +0900

    feat: add movieinfo poster to movielist.js

--- src/components/Movies/MovieList.js
+++ src/components/Movies/MovieList.js
@@ -3,7 +3,7 @@ import MovieInfo from './MovieInfo';
 const MovieList = (props) => (
   <ul>
     {props.movies.map((movie) => (
-      <MovieInfo key={movie.id} title={movie.title} genre={movie.genre} rank={movie.rank} />
+      <MovieInfo key={movie.id} poster={movie.poster} title={movie.title} genre={movie.genre} rank={movie.rank} />
     ))}
   </ul>
 );
