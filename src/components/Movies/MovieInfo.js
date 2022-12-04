1fe775cfb225f3182563ef9b0d42ed76f03efa41
yerinjo <dpfls5645@khu.ac.kr>
2022-12-04 14:20:35 +0900

    fix: change poster->img #movieinfo/list.js

--- src/components/Movies/MovieInfo.js
+++ src/components/Movies/MovieInfo.js
@@ -1,9 +1,13 @@
 import './movieinfo.css';
 
+// import styled from 'styled-components';
+
 const MovieInfo = (props) => (
   <div className="movie-info">
     <li className="movie-list">
-      <div className="movie-poster">{props.poster}</div>
+      <div className="movie-poster">
+        <img alt="movieIMG" src={props.img} />
+      </div>
       <h2 className="movie-title">{props.title}</h2>
       <div className="movie-genre">{props.genre}</div>
       <div className="movie-rank">{props.rank}</div>
