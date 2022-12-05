5e76bb6c7cc502d35540af3572c78c106828daec
yerinjo <dpfls5645@khu.ac.kr>
2022-12-05 19:16:21 +0900

    fix: add ul className #movielist.js

--- src/components/Movies/MovieList.js
+++ src/components/Movies/MovieList.js
@@ -1,7 +1,9 @@
+import './movielist.css';
+
 import MovieInfo from './MovieInfo';
 
 const MovieList = (props) => (
-  <ul>
+  <ul className="movie-list">
     {props.movies.map((movie) => (
       <MovieInfo key={movie.id} img={movie.img} title={movie.title} genre={movie.genre} rank={movie.rank} />
     ))}
