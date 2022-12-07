1fa323e88c0c556ccbbf99a461f37a98f37bbb39
yoonsaeng <truestar1997@gmail.com>
2022-12-07 20:45:44 +0900

    feat(Home): modified Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,27 @@
-import Header from '../components/Header/Header';
-import Slider from '../components/Slider/Slider';
+import { useState } from 'react';
 
-const Home = (props) => (
-  <div>
-    <Header />
-    <Slider movies={props.movies} />
-  </div>
-);
+import Header from '../components/Header/Header';
+import MovieInput from '../components/Movies/MovieInput';
+import MovieList from '../components/Movies/MovieList';
+
+const Home = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+
+  const enteredTitleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const filterTitle = props.movies.filter((p) =>
+    p.title.replace(' ', '').toLocaleLowerCase().includes(enteredTitle.toLocaleLowerCase().replace(' ', '')),
+  );
+
+  return (
+    <div>
+      <Header />
+      <MovieInput search={enteredTitle} onSearch={enteredTitleChangeHandler} />
+      <MovieList movies={filterTitle} />
+    </div>
+  );
+};
 
 export default Home;
