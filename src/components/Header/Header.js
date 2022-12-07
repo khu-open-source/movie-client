79ec56e50526294502b1ff2830a111f9bbc61e9f
yoonsaeng <truestar1997@gmail.com>
2022-12-07 20:41:04 +0900

    feat(Header): modified Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,47 +1,46 @@
 import './Header.css';
 
 import { Link } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 const Header = () => (
-  <header>
-    <div className="header-option">
-      <Link to="/SignIn">
-        <option>Sign In</option>
-      </Link>
-      <Link to="/SignUp">
-        <option>Sign Up</option>
-      </Link>
-      <Link to="/">
-        <option>Log Out</option>
-      </Link>
-    </div>
-    <nav id="nav3">
-      <Link to="/">
-        <h1 className="homebtn">OSS-Project</h1>
-      </Link>
-      <ul className="header-ul">
-        <Link to="/popular">
-          <li>Popular</li>
+  <RecoilRoot>
+    <header>
+      <div className="header-option">
+        <Link to="/SignIn">
+          <option>Sign In</option>
         </Link>
-        <Link to="/animation">
-          <li>Animation</li>
+        <Link to="/SignUp">
+          <option>Sign Up</option>
         </Link>
-        <Link to="/romance">
-          <li>Romance</li>
+        <Link to="/">
+          <option>Log Out</option>
         </Link>
-        <Link to="/science">
-          <li>Science Fiction</li>
+      </div>
+      <nav id="nav3">
+        <Link to="/">
+          <h1 className="homebtn">OSS-Project</h1>
         </Link>
-        <Link to="/action">
-          <li>Action</li>
-        </Link>
-      </ul>
-      <form>
-        <input type="text" />
-        <button type="submit">Search</button>
-      </form>
-    </nav>
-  </header>
+        <ul className="header-ul">
+          <Link to="/popular">
+            <li>Popular</li>
+          </Link>
+          <Link to="/fantasy">
+            <li>Fantasy</li>
+          </Link>
+          <Link to="/romance">
+            <li>Romance</li>
+          </Link>
+          <Link to="/science">
+            <li>Science Fiction</li>
+          </Link>
+          <Link to="/action">
+            <li>Action</li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  </RecoilRoot>
 );
 
 export default Header;
