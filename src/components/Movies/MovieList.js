9883345f1476c01ab9c7878c6db2e227e991dfea
yoonsaeng <truestar1997@gmail.com>
2022-12-07 20:43:33 +0900

    feat(MovieList): modified MovieList.js

--- src/components/Movies/MovieList.js
+++ src/components/Movies/MovieList.js
@@ -1,11 +1,20 @@
-import './movielist.css';
+import './MovieList.css';
 
 import MovieInfo from './MovieInfo';
 
 const MovieList = (props) => (
   <ul className="movie-list">
     {props.movies.map((movie) => (
-      <MovieInfo key={movie.id} img={movie.img} title={movie.title} genre={movie.genre} rank={movie.rank} />
+      <MovieInfo
+        key={movie.id}
+        title={movie.title}
+        genre={movie.genreList}
+        popularity={movie.popularity}
+        poster={movie.poster_path}
+        overview={movie.overview}
+        date={movie.release_date}
+        adult={movie.adult}
+      />
     ))}
   </ul>
 );
