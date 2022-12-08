fb8cd721b090382fec29327984278e39c9ed1ec1
L2HYUNN <donghyuni98@gmail.com>
2022-12-08 20:39:57 +0900

    feat(movieinfo): add MovieInfo search css

--- src/components/Movies/MovieInput.js
+++ src/components/Movies/MovieInput.js
@@ -2,7 +2,7 @@ import './MovieInput.css';
 
 const MovieInput = (props) => (
   <form id="nav3" className="movieInput">
-    <input type="text" value={props.search} onChange={props.onSearch} />
+    <input type="text" value={props.search} onChange={props.onSearch} className="movie-boxx" />
   </form>
 );
 
